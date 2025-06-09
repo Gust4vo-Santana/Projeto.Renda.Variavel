c77a37bf57c0eb120e8aedaa164ffff487577c66
Gust4vo-Santana <gustavosantanasantos329@gmail.com>
2025-06-08 21:54:14 -0300

    feat: create use cases installer

--- Application/Shared/Installers/UseCasesInstaller.cs
+++ Application/Shared/Installers/UseCasesInstaller.cs
@@ -1,4 +1,13 @@
-﻿using Application.UseCases.User.GetTotalInvestedByAssetUseCase;
+﻿using Application.UseCases.Operation.GetBrokerageFeeByUser;
+using Application.UseCases.Operation.GetGlobalAveragePriceByAsset;
+using Application.UseCases.Operation.GetTopBrokerageFeePayers;
+using Application.UseCases.Operation.GetTotalBrokerageFee;
+using Application.UseCases.Position.GetAveragePriceByUser;
+using Application.UseCases.Position.GetGlobalPosition;
+using Application.UseCases.Position.GetPositionByAsset;
+using Application.UseCases.Position.GetTopUsersWithHighestPositions;
+using Application.UseCases.Position.GetTotalInvestedByAsset;
+using Application.UseCases.Quote;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Application.Shared.Installers
@@ -7,7 +16,17 @@ namespace Application.Shared.Installers
     {
         public static IServiceCollection AddUseCases(this IServiceCollection services)
         {
-            services.AddGetTotalInvestedByAssetUseCase();
+            services.AddGetTotalInvestedByAssetUseCase()
+                    .AddGetGlobalPositionUseCase()
+                    .AddGetLatestQuoteUseCase()
+                    .AddGetAveragePriceUseCase()
+                    .AddGetPositionByAssetUseCase()
+                    .AddGetBrokerageFeeByUserUseCase()
+                    .AddGetTotalBrokerageFeeUseCase()
+                    .AddGetGlobalAveragePriceByAssetUseCase()
+                    .AddGetTopUsersWithHighestPositionsUseCase()
+                    .AddGetTopBrokerageFeePayersUseCase();
+
             return services;
         }
     }
