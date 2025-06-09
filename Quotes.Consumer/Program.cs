37d30bdb189ad594fa0141042d6ea5ae08123f4b
Gust4vo-Santana <gustavosantanasantos329@gmail.com>
2025-06-09 09:54:39 -0300

    build: add mySql dependency injection to worker

--- Quotes.Consumer/Program.cs
+++ Quotes.Consumer/Program.cs
@@ -1,8 +1,10 @@
 using Application.UseCases.Quote.AddNewQuote;
+using Infrastructure.MySql.Installers;
 using Quotes.Consumer.AddNewQuoteWorkerService;
 
 var builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddHostedService<Worker>();
+builder.Services.AddMySql(builder.Configuration);
 builder.Services.AddAddNewQuoteUseCase();
 
 var host = builder.Build();
